b2c4d5330e3fab8cfc49e439c1110af04dc7250a
Rocio Alcala <rro.alcala@gmail.com>
2024-05-19 20:38:39 -0300

    style: tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,17 +4,26 @@ const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))"
       },
-    },
+      fontFamily: {
+        vidaloka: ["var(--font-vidaloka)"],
+        inconsolata: ["var(--font-inconsolata)"],
+        poppins: ["var(--font-poppins)"],
+        manrope: ["var(--font-manrope)"],
+        mulish: ["var(--font-mulish)"],
+        roboto_mono: ["var(--font-roboto_mono)"],
+        "comic-cat": ["comic-cat"]
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
 export default config;
