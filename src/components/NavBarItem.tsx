b20c7272afc1d0b7bf19a44cfb0907f9131fc252
Rocio Alcala <rro.alcala@gmail.com>
2024-05-24 21:52:00 -0300

    chore: NavBarItem component

---
+++ src/components/NavBarItem.tsx
@@ -0,0 +1,28 @@
+import { PropsWithChildren } from "react";
+import { IconType } from "react-icons";
+
+interface NavBarItemProps {
+  onClick?: () => void;
+  active?: boolean;
+  Icon: IconType;
+}
+
+export default function NavBarItem({
+  onClick,
+  children,
+  active,
+  Icon,
+}: NavBarItemProps & PropsWithChildren) {
+  return (
+    <li
+      onClick={onClick}
+      className={`navbar-item transition-* group m-2 flex items-center gap-2 font-mulish text-xl font-bold duration-300 hover:cursor-pointer hover:text-yellow-400 ${active && "text-yellow-400"}`}
+    >
+      <Icon
+        size={35}
+        className="transition-* text-white duration-300 group-hover:rotate-6 group-hover:scale-110"
+      />
+      {children}
+    </li>
+  );
+}
