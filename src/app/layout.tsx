ffa0e9b1de66fbaeadb4524df27b5bb03906bf93
Rocio Alcala <rro.alcala@gmail.com>
2024-05-24 21:58:32 -0300

    chore: adding fonts to body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,35 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import {
+  inconsolata,
+  manrope,
+  mulish,
+  poppins,
+  vidaloka,
+} from "@/assets/fonts";
+import NavBar from "@/components/NavBar";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Kokekosas",
-  description: "Kokedamas de autor"
+  description: "Kokedamas de autor",
 };
 
 export default function RootLayout({
-  children
+  children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <nav></nav>
+      <body
+        className={`${vidaloka.variable} ${mulish.variable}  ${manrope.variable} ${poppins.variable} ${inconsolata.variable} font-inconsolata`}
+      >
         {children}
+        <Footer />
       </body>
     </html>
   );
