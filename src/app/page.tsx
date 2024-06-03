f0bd67bc08b780f9f737eedee816fbb28f811d3b
Rocio Alcala <rro.alcala@gmail.com>
2024-06-03 19:04:09 -0300

    style: hero background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
-"use client";
-
 import Button from "@/components/Button";
+import InfiniteCarousel from "@/components/InfiniteCarousel";
 import NavBar from "@/components/NavBar";
 
 export default function Home() {
@@ -9,7 +8,10 @@ export default function Home() {
       <NavBar />
       <main className="min-h-svh">
         <section
-          className="flex h-svh flex-col bg-black px-6 pt-64 sm:px-20"
+          style={{
+            backgroundImage: "url('/monstera.jpg')",
+          }}
+          className="flex h-svh flex-col bg-cover px-6 pt-64 sm:px-20"
           id="home"
         >
           <h1 className="shrink text-6xl font-extrabold tracking-widest text-white sm:text-8xl lg:text-9xl">
@@ -17,7 +19,9 @@ export default function Home() {
           </h1>
           <Button className="w-fit text-white"> ENCARGA TU KOKEDAMA </Button>
         </section>
-        <section id="kokedama" className=" h-svh bg-yellow-300"></section>
+        <section id="kokedama" className=" h-svh bg-black">
+          <InfiniteCarousel />
+        </section>
         <section id="cuidados" className="h-svh bg-slate-500"></section>
         <section id="productos" className="h-svh bg-stone-800"></section>
         <section id="contacto" className="h-svh bg-red-600"></section>
