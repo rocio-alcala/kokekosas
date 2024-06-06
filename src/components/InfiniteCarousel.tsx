9000290288cc1e0bab12ee3e6aa64922d8a2e28b
Rocio Alcala <rro.alcala@gmail.com>
2024-06-06 13:38:30 -0300

    fix: infinite carousel keys

--- src/components/InfiniteCarousel.tsx
+++ src/components/InfiniteCarousel.tsx
@@ -29,16 +29,22 @@ export default function InfiniteCarousel() {
   return (
     <div className="group flex overflow-hidden hover:animate-none">
       <div className="group-hover:pauseAnimation flex animate-infiniteScroll flex-nowrap bg-black">
-        {carouselText.map((text) => (
-          <span className="p-5 text-xl font-bold text-white" key={text}>
+        {carouselText.map((text, index) => (
+          <span
+            className="p-5 text-xl font-bold text-white"
+            key={text + index + "original"}
+          >
             {text}
           </span>
         ))}
       </div>
       {/* copia para que se cubra el espacio desplazado hasta que se vuelve a iniciar la animacion */}
       <div className="group-hover:pauseAnimation flex animate-infiniteScroll flex-nowrap bg-black">
-        {carouselText.map((text) => (
-          <span className="p-5 text-xl font-bold text-white" key={text}>
+        {carouselText.map((text, index) => (
+          <span
+            className="p-5 text-xl font-bold text-white"
+            key={text + index + "copy"}
+          >
             {text}
           </span>
         ))}
