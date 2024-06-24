be66048f3eccef5d886b505e1ef26323f8a6c51e
Rocio Alcala <rro.alcala@gmail.com>
2024-06-24 19:13:02 -0300

    cuidados card styling

--- src/components/CuidadosSection.tsx
+++ src/components/CuidadosSection.tsx
@@ -9,22 +9,26 @@ const kokedamaCuidados = [
   {
     title: "Riego",
     texts: [
-      "El riego es esencial para mantener tus kokedamas saludables y vibrantes, para regarlas sumerge la kokedama completamente en un recipiente con agua a temperatura ambiente. Deja que absorba agua durante 10-15 minutos, hasta que deje de burbujear. Luego, deja que escurra el exceso de agua antes de volver a colocarla en su lugar",
-      "La frecuencia dependera del tipo de planta, de la ubicacion de la misma y de la epoca del ano en la que nos encontremos. Intenta siempre mantener la bola de tierra humeda.",
+      "El riego es esencial para mantener tus kokedamas saludables y vibrantes. Para regarlas, sumerge la kokedama completamente en un recipiente con agua a temperatura ambiente. Deja que absorba agua durante 10-15 minutos, hasta que deje de burbujear. Luego, deja que escurra el exceso de agua antes de volver a colocarla en su lugar.",
+      "La frecuencia dependerá del tipo de planta, de la ubicación de la misma y de la época del año en la que nos encontremos. Intenta siempre mantener la bola de tierra húmeda.",
     ],
     imgPath: "/kokedama-riego-4.jpg",
   },
   {
     title: "Ubicacion",
     texts: [
-      "Para que tus kokedamas prosperen, es importante proporcionarles el entorno adecuado, Coloca tus kokedamas en un lugar donde reciban luz indirecta brillante. Evita la luz solar directa, ya que puede secar el musgo y dañar las raíces de la planta, prefieren temperaturas entre 18°C y 25°C. Mantene tus plantas alejadas de corrientes de aire frío y fuentes de calor como radiadores.",
+      "Para que tus kokedamas prosperen, es importante proporcionarles el entorno adecuado.",
+      "Coloca tus kokedamas en un lugar donde reciban luz indirecta brillante. Evita la luz solar directa, ya que puede secar el musgo y dañar las raíces de la planta. Preferentemente, la temperatura debería mantenerse entre 18°C y 25°C.",
+      "Tip: Mantenlas alejadas de corrientes de aire frío y fuentes de calor como radiadores.",
     ],
     imgPath: "/kokedama-home-5.jpg",
   },
   {
     title: "Mantenimiento",
     texts: [
-      "El mantenimiento regular es clave para que tus kokedamas se mantengan en óptimas condiciones a lo largo del tiempo, Alimenta tus kokedamas con un fertilizante líquido diluido una vez al mes durante la temporada de crecimiento (primavera y verano). Evita el exceso de fertilizante, ya que puede quemar las raíces, Con el tiempo, el musgo exterior puede deteriorarse. Rocía el musgo con agua entre riegos para mantener la humedad, especialmente en climas secos o durante el invierno cuando la calefacción puede resecar el ambiente",
+      "El mantenimiento regular es clave para que tus kokedamas se mantengan en óptimas condiciones a lo largo del tiempo.",
+      "Alimenta tus kokedamas con un fertilizante líquido diluido una vez al mes durante la temporada de crecimiento (primavera y verano). Evita el exceso de fertilizante, ya que puede quemar las raíces.",
+      "Con el tiempo, el musgo exterior puede deteriorarse. Rocía el musgo con agua entre riegos para mantener la humedad, especialmente en climas secos o durante el invierno cuando la calefacción puede resecar el ambiente.",
     ],
     imgPath: "/kokedama-care.jpg",
   },
@@ -41,7 +45,7 @@ export default function CuidadosSection() {
       </Reveal>
       <div
         className={cn(
-          "flex h-full flex-col items-center justify-center gap-5 font-manrope transition-all duration-500 md:py-20",
+          "flex h-full flex-col items-center justify-center gap-5 font-manrope transition-all duration-500 md:py-16",
         )}
       >
         {kokedamaCuidados.map((cuidado) => (
