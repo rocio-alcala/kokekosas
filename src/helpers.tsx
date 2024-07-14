bb8ec5276080b04bb6643cf4f744b1bc305b8c53
Rocio Alcala <rro.alcala@gmail.com>
2024-07-14 19:52:04 -0300

    helper function

--- src/helpers.tsx
+++ src/helpers.tsx
@@ -7,3 +7,23 @@ export function isRunningOnClient() {
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(...inputs));
 }
+
+export function getScrollbarWidth() {
+  // Crear un elemento div temporal
+  const outer = document.createElement("div");
+  outer.style.visibility = "hidden";
+  outer.style.overflow = "scroll"; // Forzar barra de scroll
+  document.body.appendChild(outer);
+
+  // Crear un hijo dentro del div temporal
+  const inner = document.createElement("div");
+  outer.appendChild(inner);
+
+  // Calcular la diferencia entre el ancho del div externo e interno
+  const scrollbarWidth = outer.offsetWidth - inner.offsetWidth;
+
+  // Eliminar los divs temporales
+  if (outer.parentNode) outer.parentNode.removeChild(outer);
+
+  return scrollbarWidth;
+}
